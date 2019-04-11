d8052c2c9aa3c90a022f2f9f39eaf3414daa8b88
cecilean <cecilialeandro@gmail.com>
2019-04-10 20:48:40 -0400

    Adding a working counter app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from 'react';
 
-class App extends Component {
-  render() {
-    return (
-
-    );
+class Counter extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 0,
+    }
   }
-}
 
-export default App;
+  render (){
+    return (
+      <div className="container">
+              <div className="navbar">Counter.js</div>
+                <div className="counter">
+                  <h1>What do we put here</h1>
+                  <button type="button">Increment</button>
+                  <button type="button">Decrement</button>
+                </div>
+              </div>
+
+    )
+  }
+
+}
+export default Counter;
